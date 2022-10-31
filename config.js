ad1f5a8e7b10cd7aa5c74c770764fa8546ae0874
phap230901 <86772189+phap230901@users.noreply.github.com>
2022-10-31 10:37:46 +0700

    ưdwdw
    ưdw

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
+    titleWeb: "Tiêu đề frweb",
+    introTitle: 'Tên người frfyêu bạn',
+    introDesc: `Trái đấfrft vốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
