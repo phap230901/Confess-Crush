9f7e8611cb78e1d316bb897bbef1667843ba7c33
phap230901 <86772189+phap230901@users.noreply.github.com>
2022-10-31 10:32:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 const CONFIG = {
-    titleWeb: "Tiêu đề web",
-    introTitle: 'Tên người yêu bạn',
-    introDesc: `Trái đất vốn lạ thường
+    titleWeb: "Tiêu đfdfd web",
+    introTitle: 'Tên ngưdfdfời yêu bạn',
+    introDesc: `Trái đất fdfdfdvốn lạ thường
     Mà sao em cứ đi nhầm đường
     Lạc vào tim anh lẻ loi
     Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
+    btnIntro: '^^HiHdfdfi^^',
     title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
     desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích lắm <33',
